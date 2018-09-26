c9341899bbfe55b4517d118a84807c439fa6242d
James <james@examples.com>
2018-09-26 17:18:45 +0900

    modified javascript

--- blog/static/js/creative.js
+++ blog/static/js/creative.js
@@ -74,11 +74,27 @@
   });
 
   // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
+  $('.popup-gallery').magnificPopup({ // '.popup-gallery, .popup-youtube'
     delegate: 'a',
-    type: 'image',
     tLoading: 'Loading image #%curr%...',
     mainClass: 'mfp-img-mobile',
+
+    type: 'iframe',
+    iframe: {
+	    	markup: '<div class="mfp-iframe-scaler">'+
+            		'<div class="mfp-close"></div>'+
+            		'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+            		'</div>',
+            patterns: {
+                youtube: {
+	              index: 'youtube.com/',
+	              id: 'v=',
+	              src: '//www.youtube.com/embed/%id%?autoplay=1'
+		        }
+		    },
+		    srcAction: 'iframe_src'
+     },
+
     gallery: {
       enabled: true,
       navigateByImgClick: true,

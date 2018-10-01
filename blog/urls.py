8327833fd3896c0641a67ac55770cc3327b1e58b
Seongho, Jin <seonghojin3@gmail.com>
2018-10-01 12:46:20 +0900

    name modified.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
 from . import views
 from django.urls import path
 
 urlpatterns = [
     # url(r'^$', views.post_list, name='post_list'),
     path('', views.base, name='base'),
+    # path('', view_uproad.uproad, name='uproad'),
 ]
